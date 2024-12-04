ee065d41d21bc5e19c2d10fd78bb06cea995d015
Cas Visser <crbcvisser@gmail.com>
2024-12-04 08:18:43 +0100

    cleaned up day 4

--- day4.py
+++ day4.py
@@ -19,118 +19,18 @@ part2 = None
 
 ### BEGIN SOLUTION
 
-part1 = 0
+grid = {complex(x, y): c for y, line in enumerate(inp.split("\n"))
+                         for x, c in enumerate(line)}
 
-xmas = "XMAS"
+# Directions: right, right up, up, left up, left, left down, down, right down
+part1 = sum("XMAS" == "".join(grid[xy + d * i] for i in range(len("XMAS")) if xy + d * i in grid)
+            for d in (1, 1 - 1j, -1j, -1 -1j, -1, -1 + 1j, 1j, 1 + 1j) 
+            for xy in grid if grid[xy] == "X")
 
-grid = [list(line) for line in inp.split("\n")]
-h = len(grid)
-w = len(grid[0])
-
-for y, row in enumerate(grid):
-    for x, c in enumerate(row):
-        if c == "X":
-            # right
-            for i in range(1, len(xmas)):
-                new_x = x + i
-                if new_x >= w:
-                    break
-                if grid[y][new_x] != xmas[i]:
-                    break
-            else:
-                part1 += 1
-            # left
-            for i in range(1, len(xmas)):
-                new_x = x - i
-                if new_x < 0:
-                    break
-                if grid[y][new_x] != xmas[i]:
-                    break
-            else:
-                part1 += 1
-            # up
-            for i in range(1, len(xmas)):
-                new_y = y - i
-                if new_y < 0:
-                    break
-                if grid[new_y][x] != xmas[i]:
-                    break
-            else:
-                part1 += 1
-            # right
-            for i in range(1, len(xmas)):
-                new_y = y + i
-                if new_y >= h:
-                    break
-                if grid[new_y][x] != xmas[i]:
-                    break
-            else:
-                part1 += 1
-
-            # diag / up
-            for i in range(1, len(xmas)):
-                new_x = x + i
-                new_y = y - i
-                if new_x >= w or new_y < 0:
-                    break
-                if grid[new_y][new_x] != xmas[i]:
-                    break
-            else:
-                part1 += 1
-            # diag / down
-            for i in range(1, len(xmas)):
-                new_x = x - i
-                new_y = y + i
-                if new_x < 0 or new_y >= h:
-                    break
-                if grid[new_y][new_x] != xmas[i]:
-                    break
-            else:
-                part1 += 1
-            # diag \ up
-            for i in range(1, len(xmas)):
-                new_x = x - i
-                new_y = y - i
-                if new_x < 0 or new_y < 0:
-                    break
-                if grid[new_y][new_x] != xmas[i]:
-                    break
-            else:
-                part1 += 1
-            # diag \ down
-            for i in range(1, len(xmas)):
-                new_x = x + i
-                new_y = y + i
-                if new_x >= w or new_y >= h:
-                    break
-                if grid[new_y][new_x] != xmas[i]:
-                    break
-            else:
-                part1 += 1
-
-part2 = 0
-for y, row in enumerate(grid):
-    for x, c in enumerate(row):
-        if c != "A":
-            continue
-
-        tl = (x - 1, y - 1)
-        tr = (x + 1, y - 1)
-        bl = (x - 1, y + 1)
-        br = (x + 1, y + 1)
-
-        invalid = False
-        for coords in [tl, tr, bl, br]:
-            if not (0 <= coords[0] < w and 0 <= coords[1] < h):
-                invalid = True
-        if invalid:
-            continue
-
-        if (
-            ((grid[tl[1]][tl[0]] == "M" and grid[br[1]][br[0]] == "S") or (grid[tl[1]][tl[0]] == "S" and grid[br[1]][br[0]] == "M"))
-            and ((grid[tr[1]][tr[0]] == "M" and grid[bl[1]][bl[0]] == "S") or (grid[tr[1]][tr[0]] == "S" and grid[bl[1]][bl[0]] == "M"))
-            ):
-            part2 += 1
+# Corners: (bottom right, top left), (bottom left, top right)
+part2 = sum(all(set("MS") == set(grid[xy + d] for d in dirs if xy + d in grid) 
+                for dirs in ((1 + 1j, -1 - 1j), (-1 + 1j, 1 - 1j))) 
+            for xy in grid if grid[xy] == "A")
 
 ### END SOLUTION
 
