88f46621abff17a1d1c383747046ad5a431c957f
Cas Visser <crbcvisser@gmail.com>
2024-12-06 09:08:34 +0100

    cleaned up day 6

--- day6.py
+++ day6.py
@@ -17,54 +17,26 @@ if len(sys.argv) > 1 and (sys.argv[1] == "gd" or sys.argv[1] == "s1" or sys.argv
 
 ### BEGIN SOLUTION
 
-from collections import defaultdict
-
 grid = {complex(x, y): c for y, line in enumerate(inp.split("\n"))
                          for x, c in enumerate(line)}
 start = [xy for xy in grid if grid[xy] == "^"][0]
-pos = start
 
-d = -1j # start by going up
-visited = set()
-path = []
-while True:
-    visited.add(pos)
-    path.append((pos, d))
-    new_pos = pos + d
-    if new_pos not in grid:
-        break
-    if grid[new_pos] != "#":
-        pos = new_pos
-        continue
-    d *= 1j
-
-part1 = len(visited) 
-
-part2 = 0
-new_obstacles = set()
-for i in range(len(path)):
-    pos, d = path[i]
-    new_obstacle = pos + d
-    if new_obstacle not in grid or grid[new_obstacle] == "#":
-        continue 
-    # Check if we enter a loop if the next cell were obstructed
+def walk(grid):
+    d = -1j # start by going up
     seen = set()
     pos = start
-    d = -1j
-    while True:
+    while pos in grid and (pos, d) not in seen:
         seen.add((pos, d))
         new_pos = pos + d
-        if new_pos not in grid:
-            break
-        if (new_pos, d) in seen:
-            new_obstacles.add(new_obstacle)
-            break
-        if grid[new_pos] != "#" and new_pos != new_obstacle:
-            pos = new_pos
+        if grid.get(new_pos) == "#":
+            d *= 1j
             continue
-        d *= 1j
+        pos = new_pos
+    return {pos for pos, _ in seen}, (pos, d) in seen
 
-part2 = len(new_obstacles)
+visited, _ = walk(grid)
+part1 = len(visited) 
+part2 = sum(walk(grid | {pos: "#"})[1] for pos in visited)
 
 ### END SOLUTION
 
