817a393b6e626b901843dbe10dd8e7387aae8bf9
Cas Visser <crbcvisser@gmail.com>
2024-12-08 07:06:42 +0100

    cleaned up day 8

--- day8.py
+++ day8.py
@@ -20,8 +20,8 @@ if len(sys.argv) > 1 and (sys.argv[1] == "gd" or sys.argv[1] == "s1" or sys.argv
 ### BEGIN SOLUTION
 
 from collections import defaultdict
+from itertools import combinations
 
-part1 = part2 = 0
 antennas = defaultdict(list)
 for y, line in enumerate(inp.split("\n")):
     for x, c in enumerate(line):
@@ -31,22 +31,20 @@ for y, line in enumerate(inp.split("\n")):
 
 w = inp.find("\n")
 h = (len(inp) // w + 1) - 1
-from itertools import combinations
-antinodes = set()
-for _f, xys in antennas.items():
+antinodes1 = set()
+antinodes2 = set()
+for xys in antennas.values():
     for xy1, xy2 in combinations(xys, 2):
         d = xy1 - xy2
-        antinodes |= {xy for xy in [xy1 + d, xy2 - d] if 0 <= xy.real < w and 0 <= xy.imag < h}
+        get_antinodes = lambda values: {xy
+                                        for i in values 
+                                        for xy in [xy1 + (d * i), xy2 - (d * i)]
+                                        if 0 <= xy.real < w and 0 <= xy.imag < h}
+        antinodes1 |= get_antinodes([1])
+        antinodes2 |= get_antinodes(range(w))
 
-part1 = len(antinodes)
-
-antinodes = set()
-for _f, xys in antennas.items():
-    for xy1, xy2 in combinations(xys, 2):
-        d = xy1 - xy2
-        antinodes |= {xy for i in range(100) for xy in [xy1 + (d * i), xy2 - (d * i)] if 0 <= xy.real < w and 0 <= xy.imag < h}
-
-part2 = len(antinodes)
+part1 = len(antinodes1)
+part2 = len(antinodes2)
 
 ### END SOLUTION
 
