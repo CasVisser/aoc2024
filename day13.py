0b49c4046f55adfe165163653d8bf98f76c68a7b
Cas Visser <crbcvisser@gmail.com>
2024-12-13 07:42:36 +0100

    solved day 13

--- day13.py
+++ day13.py
@@ -23,44 +23,23 @@ if len(sys.argv) > 1 and (sys.argv[1] == "gd" or sys.argv[1] == "s1" or sys.argv
 ### BEGIN SOLUTION
 
 import re
-from sys import setrecursionlimit
-from collections import defaultdict
-from functools import cache
-setrecursionlimit(10000)
-from math import inf
-
-def bins(goal, b, l=0, h=100):
-    if l > h:
-        return None
-    guess = (l + h) // 2
-    new_goal = goal - (guess * b)
-    if new_goal == 0:
-        return guess
-    if new_goal.real > 0 and new_goal.imag < 0 or new_goal.real < 0 and new_goal.imag > 0:
-        return None
-    if new_goal.real > 0:
-        return bins(goal, b, l=guess+1, h=h)
-    if new_goal.real < 0:
-        return bins(goal, b, l=l, h=guess-1)
 
 part1 = part2 = 0
 for machine in inp.split("\n\n"):
-    a_x, a_y, b_x, b_y, x, y = map(int, re.findall(
+    ax, ay, bx, by, gx, gy = map(int, re.findall(
             r".*A: X\+(\d+), Y\+(\d+).*B: X\+(\d+), Y\+(\d+).*X=(\d+), Y=(\d+)", 
             machine, 
             flags=re.DOTALL)[0])
-    a = complex(a_x, a_y)
-    b = complex(b_x, b_y)
-    goal = complex(x, y)
-    min_cost = inf
-    for i in range(101):
-        cur_goal = goal - i * a
-        d_b = bins(cur_goal, b)
-        if d_b is not None:
-            min_cost = min(min_cost, 3 * i + d_b)
-    if min_cost < inf:
-        part1 += min_cost
-        
+    a = (gx - (bx * gy) / by) / (ax - (ay * bx) / by)
+    b = (gy - a * ay) / by
+    if abs(round(a) - a) < .01 and abs(round(b) - b) < .01:
+        part1 += int(3 * round(a) + round(b))
+    gx += 10000000000000
+    gy += 10000000000000
+    a = (gx - (bx * gy) / by) / (ax - (ay * bx) / by)
+    b = (gy - a * ay) / by
+    if abs(round(a) - a) < .01 and abs(round(b) - b) < .01:
+        part2 += int(3 * round(a) + round(b))
 
 ### END SOLUTION
 
