eb87fe6ed0688601f3aefcc7a0b6b9dd18810738
Cas Visser <crbcvisser@gmail.com>
2024-12-18 17:14:18 +0100

    solved day 17 part 1

---
+++ day17.py
@@ -0,0 +1,58 @@
+import aocd, sys
+
+inp = r"""Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0"""
+
+# Get input from server if specified
+if len(sys.argv) > 1 and (sys.argv[1] == "gd" or sys.argv[1] == "s1" or sys.argv[1] == "s2"):
+    inp = aocd.get_data(day=17, year=2024)
+
+### BEGIN SOLUTION
+
+import re
+
+part1 = part2 = 0
+
+def combo(op, A, B, C):
+    if op < 4:
+        return op
+    return [A, B, C][op - 4]
+
+A, B, C, *prog = map(int, re.findall(r"\d+", inp))
+out = []
+pc = 0
+while pc < len(prog) - 1:
+    ins, op = prog[pc : pc + 2]
+    if ins == 0: # adv
+        A = A // 2**(combo(op, A, B, C))
+    elif ins == 1: # blx
+        B ^= op
+    elif ins == 2: # bst
+        B = combo(op, A, B, C) & 7
+    elif ins == 3 and A != 0: # jnz
+        pc = op
+        continue
+    elif ins == 4: # bxc
+        B = B ^ C
+    elif ins == 5: # out
+        out.append(combo(op, A, B, C) & 7)
+    elif ins == 6: # bdv
+        B = A // 2**(combo(op, A, B, C))
+    elif ins == 7: # cdv
+        C = A // 2**(combo(op, A, B, C))
+    pc += 2
+
+part1 = ",".join(map(str, out))
+
+### END SOLUTION
+
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
+
+if len(sys.argv) > 1 and sys.argv[1] == "s1" and input("Submit part 1? (y/n) ").lower() == "y":
+    aocd.submit(part1, part="a", day=17, year=2024)
+if len(sys.argv) > 1 and sys.argv[1] == "s2" and input("Submit part 2? (y/n) ").lower() == "y":
+    aocd.submit(part2, part="b", day=17, year=2024)
