a8c0d01bc3f8f9a1feb33e4a514d276604f0929a
Cas Visser <crbcvisser@gmail.com>
2024-12-19 14:35:26 +0100

    cleaned up day 17

--- day17.py
+++ day17.py
@@ -14,42 +14,35 @@ if len(sys.argv) > 1 and (sys.argv[1] == "gd" or sys.argv[1] == "s1" or sys.argv
 
 import re
 
-part1 = part2 = 0
-
-def combo(op, A, B, C):
-    if op < 4:
-        return op
-    return [A, B, C][op - 4]
-
 A, B, C, *prog = map(int, re.findall(r"\d+", inp))
-s = 20534812061143
-for start_A in range(s * 8, (s + 1) * 8):
-    A = start_A
+candidates = [A] + list(range(8))
+part1 = part2 = 0
+while candidates:
+    part2 = A = candidates.pop(0)
     out = []
     pc = 0
     while pc < len(prog) - 1:
         ins, op = prog[pc : pc + 2]
-        if ins == 0: # adv
-            A = A // 2**(combo(op, A, B, C))
-        elif ins == 1: # blx
-            B ^= op
-        elif ins == 2: # bst
-            B = combo(op, A, B, C) & 7
-        elif ins == 3 and A != 0: # jnz
-            pc = op
-            continue
-        elif ins == 4: # bxc
-            B = B ^ C
-        elif ins == 5: # out
-            out.append(combo(op, A, B, C) & 7)
-        elif ins == 6: # bdv
-            B = A // 2**(combo(op, A, B, C))
-        elif ins == 7: # cdv
-            C = A // 2**(combo(op, A, B, C))
+        combo = {0: 0, 1: 1, 2: 2, 3: 3, 4: A, 5: B, 6: C}[op]
+        match ins:
+            case 0: A = A >> combo        # adv
+            case 1: B ^= op               # blx
+            case 2: B = combo & 7         # bst
+            case 3:                       # jnz
+                if A != 0:
+                    pc = op
+                    continue
+            case 4: B = B ^ C             # bxc
+            case 5: out.append(combo & 7) # out
+            case 6: B = A >> combo        # bdv
+            case 7: C = A >> combo        # cdv
         pc += 2
-    print(f"{start_A}: {','.join(map(str, out))}")
+    if part1 == 0:
+        part1 = ",".join(map(str, out))
     if out == prog:
-        part2 = start_A
+        break
+    if out == prog[-len(out):]:
+        candidates = list(range(part2 * 8, (part2 + 1) * 8)) + candidates
 
 ### END SOLUTION
 
